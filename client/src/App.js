9a5f59630a49875fa35985ea1f4bcb5550272c86
seonghyeon <impressor7@gmail.com>
2020-01-05 17:09:04 +0900

    update client to have router and react-helmet

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,55 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Helmet } from 'react-helmet';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-function App() {
+const IndexPage = () => {
   useEffect(() => {
     // puppeteer가 cache하는 순간을 해당 전역 변수로 판단
     window.prerenderReady = true;
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>
+          Index Page Title
+        </title>
+      </Helmet>
+      <div>Index Page</div>
+    </>
+  );
+}
+
+const HelloPage = () => {
+  useEffect(() => {
+    // puppeteer가 cache하는 순간을 해당 전역 변수로 판단
+    window.prerenderReady = true;
+  }, [])
+
+  return (
+    <>
+      <Helmet>
+        <title>
+          Hello Page Title
+        </title>
+      </Helmet>
+      <div>Hello Page</div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={IndexPage} />
+        <Route exact path="/hello" component={HelloPage} />
+      </Switch>
+    </Router>
   );
 }
 
